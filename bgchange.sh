68a8f54babe84c1f4b82b6d3d5fe5b57a0843d95
Etienne PAQUELET <116279072+e-paquelet@users.noreply.github.com>
2024-11-19 17:48:09 +0100

    Delete bgchange.sh

--- bgchange.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://img.freepik.com/psd-gratuit/ordinateur-portable-pirate-par-pirate-informatique_587448-4948.jpg -O $HOME/img.jpg
-
-gsettings set org.mate.background picture-filename $HOME/img.jpg
-
-
