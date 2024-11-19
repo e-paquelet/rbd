fcba4b0593559a77d1b5e49e3c54ad264579348a
Etienne PAQUELET <116279072+e-paquelet@users.noreply.github.com>
2024-11-19 17:34:56 +0100

    Add files via upload

--- bgchange.sh
+++ bgchange.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-wget https://img.freepik.com/photos-premium/paysage-fantastique-lac-montagnes-nuit-generative-ai-i_841229-8453.jpg -O $HOME/img.jpg
+wget https://img.freepik.com/psd-gratuit/effet-texte-psd-hacker-3d_165789-733.jpg -O $HOME/img.jpg
 
 gsettings set org.mate.background picture-filename $HOME/img.jpg
+
+
