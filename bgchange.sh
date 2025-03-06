b425dc7546e9a49f8429417592217d7ed13d417a
Etienne PAQUELET <116279072+e-paquelet@users.noreply.github.com>
2025-03-06 11:46:51 +0100

    Delete bgchange.sh
    Signed-off-by: Etienne PAQUELET <116279072+e-paquelet@users.noreply.github.com>

--- bgchange.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://img.freepik.com/psd-gratuit/effet-texte-psd-hacker-3d_165789-733.jpg -O $HOME/img2.jpg
-
-gsettings set org.mate.background picture-filename $HOME/img2.jpg
-
-
