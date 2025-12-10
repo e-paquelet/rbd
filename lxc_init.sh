8e775ba3b6f26e5d1348eba844cfb7d5e81c1f65
Etienne PAQUELET <116279072+e-paquelet@users.noreply.github.com>
2025-12-10 20:37:34 +0100

    Update lxc_init.sh
    Signed-off-by: Etienne PAQUELET <116279072+e-paquelet@users.noreply.github.com>

--- lxc_init.sh
+++ lxc_init.sh
@@ -20,4 +20,5 @@ display "Génération de la locale"
 locale-gen || error "Failed to generate locale" 3 
 sleep 5
 display "creation de l'utilisateur user"
-useradd -G sudo -m -s /bin/bash user || error "Failed to create user" 4
+password=$(openssl passwd -6 "tpRT9025")
+useradd -G sudo -p $password -m -s /bin/bash user || error "Failed to create user" 4
