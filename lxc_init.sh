e9a8f1f095bf86b60d6ab23ec8c52de90cad1acb
Etienne PAQUELET <116279072+e-paquelet@users.noreply.github.com>
2025-12-10 19:02:47 +0100

    Update lxc_init.sh
    Signed-off-by: Etienne PAQUELET <116279072+e-paquelet@users.noreply.github.com>

--- lxc_init.sh
+++ lxc_init.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-sed -i 's/# fr_FR.UTF-8/fr_FR.UTF-8/' /etc/locale.gen
-echo 'LANG=fr_FR.UTF-8' > /etc/default/locale
-locale-gen
 
-useradd -G sudo -m -s /bin/bash user
+display() {
+	printf -- "%s\n" "$1" 
+}
+
+display_lxc() {
+	sudo lxc-ls -f
+
+}
+
+error() {
+        printf "Error : %s\n" "$1" >&2
+        exit "$2"
+}
+
+display "Configuration des locales"
+sed -i 's/# fr_FR.UTF-8/fr_FR.UTF-8/' /etc/locale.gen || error "Failed to configure locale 1" 1
+echo 'LANG=fr_FR.UTF-8' > /etc/default/locale || error "Failed to configure locale 2" 2
+display "Génération de la locale"
+locale-gen || error "Failed to generate locale" 3 
+
+useradd -G sudo -m -s /bin/bash user || error "Failed to create user" 4
